169f790d3143374a885e29b75f3eeb8d251e0289
Mh Limon <102249045+urstylelimon@users.noreply.github.com>
2023-08-20 03:33:49 +0600

    Time: 46 ms (51.06%), Space: 16.4 MB (23.32%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,12 +1,13 @@
 class Solution:
     def addDigits(self, num: int) -> int:
         result = 0
-        while True:
+        flag = True
+        while flag:
             num = list(str(num))
             for i in num:
                 result += int(i)
             num = result
             result = 0
             if len(str(num)) == 1:
-                break
+                flag = False
         return num
