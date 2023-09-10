85e4f2ac69a9c5790d1aa989cd602dba414f2281
Mh Limon <102249045+urstylelimon@users.noreply.github.com>
2023-09-10 18:03:30 +0600

    Time: 2160 ms (63.11%), Space: 55.1 MB (73.32%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -3,21 +3,18 @@ class Solution:
         if n <= 2:
             return 0
         else:
-            def check_prime(num):
-                list = [True] * (num + 1)
-                list[0] = list[1] = False
-                count = 0
+            n = n-1
+            list = [True] * (n + 1)
+            list[0] = list[1] = False
+            count = 0
 
-                for i in range(2, int(num**0.5) + 1):
-                    if list[i]:
-                        for j in range(i * i, num + 1, i):
-                            list[j] = False
+            for i in range(2, int(n ** 0.5) + 1):
+                if list[i]:
+                    for j in range(i * i, n + 1, i):
+                        list[j] = False
 
-                for k in range(2, num + 1):
-                    if list[k]:
-                        count += 1
+            for k in range(2, n + 1):
+                if list[k]:
+                    count += 1
 
-                return count
-
-            count = check_prime(n-1)
             return count
