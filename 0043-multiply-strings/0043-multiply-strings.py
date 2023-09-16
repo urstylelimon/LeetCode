e7f2f88f8072f5a695217386c3632e3d23f3540a
Mh Limon <102249045+urstylelimon@users.noreply.github.com>
2023-09-17 03:06:45 +0600

    Time: 30 ms (98.25%), Space: 16.3 MB (40.43%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,6 +1,4 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         sum = int(num1) * int(num2)
-        str_sum = str(sum)
-        return str_sum
-        
+        return str(sum)
