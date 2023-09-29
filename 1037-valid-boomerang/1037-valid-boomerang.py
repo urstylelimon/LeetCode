680ad76ed3b5ab5a1e6d2405159d7a1ac5a9ec19
Mh Limon <102249045+urstylelimon@users.noreply.github.com>
2023-09-29 19:13:18 +0600

    Time: 44 ms (24.22%), Space: 16.2 MB (57.20%) - LeetHub

---
+++ 1037-valid-boomerang/1037-valid-boomerang.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isBoomerang(self, points: List[List[int]]) -> bool:
+        x1,y1 = points[0]
+        x2,y2 = points[1]
+        x3,y3 = points[2]
+        print(y1,y2)
+        result = abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2
+        if result == 0:
+            return False
+        else:
+            return True
