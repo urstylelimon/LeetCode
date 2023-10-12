6005a386058f38610e3d28b4084a38b46d7db84e
Mh Limon <102249045+urstylelimon@users.noreply.github.com>
2023-10-12 22:45:06 +0600

    Time: 95 ms (90.00%), Space: 19.4 MB (65.11%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,3 +1,24 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        return eval(s.replace("/", "//"))
+        s = s.replace(" ", "")
+        s += '+'
+        indx = []
+        result = 0
+        op = '+'
+
+        for i in s:
+            if i.isdigit():
+                result = (result * 10) + int(i)
+            else:
+                result = int(result)
+                if op == '+':
+                    indx.append(result)
+                elif op == '-':
+                    indx.append(-result)
+                elif op == '*':
+                    indx.append(indx.pop() * result)
+                elif op == '/':
+                    indx.append(int(indx.pop() / result))
+                result = 0
+                op = i
+        return (sum(indx))
