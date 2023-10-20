da0b1a463e8e5c08b9e2408eb03f446d2cb111b0
Mh Limon <102249045+urstylelimon@users.noreply.github.com>
2023-10-20 22:03:27 +0600

    Time: 51 ms (6.34%), Space: 16.1 MB (95.97%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -1,11 +1,4 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
         import math 
-
-        if a == 0 and b != 0:
-            return b
-        elif b == 0 and a != 0:
-            return a
-        else:
-            result = int(math.log(math.exp(a) * math.exp(b)))
-            return result
+        return (int(math.log2(2**a * 2**b)))
