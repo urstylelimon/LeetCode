9ee128e856c72e1e466dfb32fe596c3308fb9e8b
Mh Limon <102249045+urstylelimon@users.noreply.github.com>
2024-04-23 23:04:24 +0600

    Time: 886 ms (26.34%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-SELECT email
-FROM Person
-GROUP BY email
-HAVING COUNT(*) > 1;
+
+SELECT DISTINCT email FROM Person
+WHERE email IN ( SELECT email FROM Person GROUP BY email HAVING COUNT(*) > 1 );
